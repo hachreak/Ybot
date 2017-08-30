ee95eb0838fb532b1a77298c422ed8d21e541806
Leonardo Rossi <leonardo.r@cern.ch>
2017-08-30 16:51:42 +0200

    menu: get all menu
    Signed-off-by: Leonardo Rossi <leonardo.r@cern.ch>

--- plugins/menu/src/ybot_plugin_menu.erl
+++ plugins/menu/src/ybot_plugin_menu.erl
@@ -10,7 +10,8 @@ start() -> ok.
 stop() -> ok.
 
 execute(_) ->
-  {ok, File} = file:read_file("/tmp/menu.html"),
+  % {ok, File} = file:read_file("/tmp/menu.html"),
+  File = get_page(),
   [_ | List] = re:split(File, "HauteurMenu", [{return,list}]),
   Seq = lists:seq(1, erlang:length(List)),
   MenuHtml = lists:filtermap(fun({Num, El}) ->
@@ -30,9 +31,25 @@ execute(_) ->
 
 format(Menu) ->
   MenuFormatted = lists:foldl(fun({Name, Price}, Acc) ->
-
       Acc ++ lists:flatten(io_lib:format(" - ~s (~s CHF)\n", [Name, Price]))
     end, "", Menu),
   Header = ":fork_and_knife: Hey Y'@/all, it's lunch time! :clock12:",
   unicode:characters_to_list(
     lists:flatten(io_lib:format("~s~n~s\n", [Header, MenuFormatted]))).
+
+get_page() ->
+  BaseUrl = "http://extranet.novae-restauration.ch",
+  Url = BaseUrl ++ "/index.php?frame=1&x=ad3f8f75fe1e353b972afcce8e375d6e&y=81dc9bdb52d04dc20036dbd8313ed055&z=135",
+  inets:start(),
+  {ok, {{"HTTP/1.1",302,"Found"}, Headers, _}} = httpc:request(
+                                get, {Url, []}, [{autoredirect, false}], []),
+  Location = proplists:get_value("location", Headers),
+  Cookie = proplists:get_value("set-cookie", Headers),
+  UrlMenu = BaseUrl ++ Location,
+  {ok, {{"HTTP/1.1",200,"OK"}, _, Body1}} = httpc:request(
+          get, {UrlMenu, [{"Cookie", Cookie}]}, [{autoredirect, false}], []),
+  {ok, {{"HTTP/1.1",200,"OK"}, _, Body2}} = httpc:request(
+      post, {UrlMenu, [{"Cookie", Cookie}],
+             "application/x-www-form-urlencoded", "fn_limite=2&fn_numpage=1"},
+      [{autoredirect, false}], []),
+  Body1 ++ Body2.
